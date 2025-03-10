82bd683e8f2b241af8afac776d39ee433414ffd8
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-03-10 13:53:50 +0300

    Creating a home page to render

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const movies = [
+    { id: 1, title: "John wick", release_date: "2020" },
+    { id: 1, title: "Terminator", release_date: "1999" },
+    { id: 1, title: "The matrix", release_date: "1998" },
+  ];
+  const handleSearch = () => {};
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search for movies"
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
