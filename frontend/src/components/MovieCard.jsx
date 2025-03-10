c372489d8a3b096bace6597f7a5543b31e8755f5
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-03-10 17:02:12 +0300

    Change class name

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,3 +1,5 @@
+import "../css/MovieCard.css";
+
 function MovieCard({ movie }) {
   function onFavoriteClick() {
     alert("clicked");
@@ -7,7 +9,7 @@ function MovieCard({ movie }) {
       <div className="movie-poster">
         <img src={movie.url} alt={movie.title} />
         <div className="movie-overlay">
-          <button className="favorite-icon" onClick={onFavoriteClick}>
+          <button className="favorite-btn" onClick={onFavoriteClick}>
             🤍
           </button>
         </div>
