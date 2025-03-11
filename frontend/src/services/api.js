8762dd6a8bc9ccafb4029a3d6740e9004d963cb9
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-03-11 15:35:41 +0300

    fetching  movies from api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,6 @@
 const API_KEY = "d0fade7cfd4647a912a6e6f218a69e1c";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-// export const getPopularMovies = async () => {
-//   const response = await fetch(`${BASE_URL}/tv/popular?api_key=${API_KEY}`);
-//   const data = response.json();
-//   return data.results;
-// };
-
 export const getPopularMovies = async () => {
   try {
     const response = await fetch(
@@ -18,7 +12,7 @@ export const getPopularMovies = async () => {
     }
 
     const data = await response.json();
-    console.log("Popular Movies:", data.results); // Log the results
+    // console.log("Popular Movies:", data.results); // Log the results
 
     return data.results; // Return the movies array
   } catch (error) {
@@ -28,12 +22,23 @@ export const getPopularMovies = async () => {
 };
 
 export const searchMovies = async (query) => {
-  const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
-      query
-    )}`
-  );
-  const data = response.json();
-  console.log(data);
-  return data.results;
+  try {
+    const response = await fetch(
+      `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${encodeURIComponent(
+        query
+      )}&page=1`
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("Search Results:", data.results);
+
+    return data.results; // Return search results
+  } catch (error) {
+    console.error("Error searching movies:", error);
+    return [];
+  }
 };
